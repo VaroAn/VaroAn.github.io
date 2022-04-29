77f4733a35b116a1e5f59f98764a8555f5894b45
VaroAn <104613102+VaroAn@users.noreply.github.com>
2022-04-29 14:16:26 -0500

    initial commit

---
+++ practica2/tienda/js/login.js
@@ -0,0 +1,26 @@
+function go(){
+    
+    if (document.form.pass.value=='123456' && document.form.email.value=='correo@correo.com'){ 
+            
+            alert ("Bienvenido");
+            window.open("index-.html");
+            window.close("login.html");     
+             
+        } 
+        else{ 
+             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+        } 
+} 
+
+function ia(){
+        alert("Primero debes iniciar sesión");
+}
+
+function ac(){
+    alert("Se agrego correctamente al carrito");
+}
+
+function comp(){
+    alert("Muchgas Gracias por la Compra");
+}
+
